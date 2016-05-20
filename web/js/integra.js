cdc4a727c75e365b96601312be9db4057478e5bb
adomingor <adomingor@hotmail.com>
2016-05-20 13:11:32 -0300

    agregando eliminar registros, poner mensaje de confirmacion

--- web/js/integra.js
+++ web/js/integra.js
@@ -3,10 +3,9 @@ $(document).ready(function() {
         // <dialog> actualmente soportada por Chrome (experimental)
         // var dialog = document.querySelector('dialog');
         // dialogPolyfill.registerDialog(dialog);
-
-        var isi_msj_popUp = document.querySelector('.mdl-js-snackbar');
-        /* Declarando variable a retornar con nuestro objeto, retornaremos "false" * en caso de algún error */
-        var objetoAjax = false;
+        var $isi_msjErrSist = "Contacte al administrador del sitio!"; // agregar email, nro telef. etc.
+        var $isi_msj_popUp = document.querySelector('.mdl-js-snackbar'); // para mostrar los mensajes snack/toast
+        var objetoAjax = false; // Declarando variable a retornar con nuestro objeto, retornaremos "false" * en caso de algún error
 
         String.prototype.capitaliza = function() { // pasa la primera letra de la primera palabra a mayúsculas
             return this.charAt(0).toUpperCase() + this.slice(1);
@@ -25,12 +24,25 @@ $(document).ready(function() {
     // FIN GLOBALES ------------------------------------------------------------------
 
 
+    function isi_abrirModal ($id) {
+        //agregar control que exista .modal sino return false
+        $("#"+$id).css({"opacity":"1", "pointer-events":"auto"});
+        return true
+    };
+    function isi_cerrarModal ($id) {
+        //agregar control que exista .modal sino return false
+        $("#"+$id).css({"opacity":"", "pointer-events":""});
+        return true
+    };
+
     $(".isi_cerrarModal").click(function() {
-        $("#"+this.name).css({"opacity":"", "pointer-events":""});
+        isi_cerrarModal(this.name);
+        // $("#"+this.name).css({"opacity":"", "pointer-events":""});
     });
 
     $(".isi_abrirModal").click(function() {
-        $("#"+this.name).css({"opacity":"1", "pointer-events":"auto"});
+        // $("#"+this.name).css({"opacity":"1", "pointer-events":"auto"});
+        isi_abrirModal(this.name);
     });
 
     /* (des)chequea un grupo de input tipo checkbox que estén habilitados (disabled = false)
@@ -38,13 +50,13 @@ $(document).ready(function() {
     y en el name el nombre del grupo de check que quiere controlar
     si no tiene name o es "" chequea todos los checks
     */
-    // controlo que posea attr name ($(this).attr("name") ? "[name="+this.name+"]" : "")
     $("input[type=checkbox].isi_chk_grupo").click(function(elemento) {
         return (tildarCheck(this.name, this.checked));
     });
     function tildarCheck($nombre, $check) { // $nombre = "[name=NombreGrupoA(des)Tildar]" o "", $check = true o false
         $nombre.length ? $name = "[name="+$nombre+"]" : $name = "";
-        $("input:checkbox"+$name+":not(:disabled)").prop('checked', $check); // check comunes
+        // $("input:checkbox"+$name+":not(:disabled)").prop('checked', $check); // check comunes
+        $("input:checkbox:not(:disabled)"+$name).prop('checked', $check); // check comunes
         $.each($("label.mdl-checkbox"+$name), function (indice, elemento) { // check mdl
             if (!elemento.firstElementChild.disabled)
                 $check ? elemento.MaterialCheckbox.check(): elemento.MaterialCheckbox.uncheck();
@@ -52,10 +64,18 @@ $(document).ready(function() {
     }
     /* Fin (des)chequea un grupo de input tipo checkbox */
 
-    // muestra un mensaje con la cantidad de checkbox seleccionados
-    $("input:checkbox").click(function(elemento){
-        $cant = $('input:checkbox:checked').size();
-        isi_msj_popUp.MaterialSnackbar.showSnackbar({message: $cant + " elementos seleccionados", timeout: 1000});
+    // muestra un mensaje con la cantidad de checkbox seleccionados que no sean cabeceras de checks
+    // si el check tildado no es cabecera y tiene name (de algun grupo posiblemente), me fijo si hay
+    // mas check con ese nombre que no sean cabecera, si no hay mas y existe una cabecera, la destildo
+    $("input:checkbox").click(function(elemento) {
+        $chks  = $('input:checkbox:checked:not(.isi_chk_grupo)');
+
+        if ($(this).attr("name")) {
+            if ($("input:checkbox:checked:not(.isi_chk_grupo):not(:disabled)[name="+this.name+"]").length == 0)
+                tildarCheck(this.name, false);
+        }
+
+        $isi_msj_popUp.MaterialSnackbar.showSnackbar({message: "Hay " + $chks.length + " elemento(s) seleccionado(s)", timeout: 1000});
     });
 
     // function alternarChkTodos() {
@@ -89,20 +109,46 @@ $(document).ready(function() {
     /*la tabla (html) debe tener:
     tr con id = value del checkbox que tiene la fila (tr)
     checkbox del tr con value = id del registro a eliminar*/
-    $(".isi_elim_reg").click(function(){
+    /* Debe existir un objeto con el name="grupo de checkbox de la tabla" y un valor = total general de registros
+        en este caso se usa un badge de mdl
+    */
+    $(".isi_elim_reg").click(function(elemento){
         // controles:
         // el objeto debe tener la propiedad name (con el nombre del grupo de checkbox) y el href a la acción del controlador
         // la fila debe estar visible (por si está activo el filtro de busqueda), el checkbox habilitado
-        // debe haber al menos un check seleccionado
+        // debe haber al menos un check seleccionado y habilitado
         if (!$(this).attr("name")) {
-            isi_msj_popUp.MaterialSnackbar.showSnackbar({message: "Imposible determinar los registros a eliminar!", timeout: 2500});
+            $isi_msj_popUp.MaterialSnackbar.showSnackbar({message: "Imposible determinar los registros a eliminar. " + $isi_msjErrSist, timeout: 2500});
             return false;
         }
         if (!$(this).attr("href")) {
-            isi_msj_popUp.MaterialSnackbar.showSnackbar({message: "Imposible ejecutar la acción!", timeout: 2500});
+            $isi_msj_popUp.MaterialSnackbar.showSnackbar({message: "Imposible ejecutar la acción. " + $isi_msjErrSist, timeout: 2500});
+            return false;
+        }
+        var $totRegi = $("span.mdl-badge[name="+this.name+"]").attr("data-badge"); //null = undefined = no hay badge
+        if(!$totRegi) {
+            $isi_msj_popUp.MaterialSnackbar.showSnackbar({message: "Imposible obtener la cantidad de registros. " + $isi_msjErrSist, timeout: 2500});
+            return false;
+        }
+        var $cantChks = $("input:checkbox:checked:not(:disabled):not(.isi_chk_grupo)[name="+this.name+"]").length;
+        if ($cantChks == 0) {
+            $isi_msj_popUp.MaterialSnackbar.showSnackbar({message: "Elija un elemento para eliminarlo", timeout: 2500});
             return false;
         }
 
+        $isi_msj_popUp.MaterialSnackbar.showSnackbar({message: "Pasa los controles", timeout: 1000});
+
+
+        isi_abrirModal("isi_div_msjConfirmacion");
+        evento.preventDefault();
+
+        if ($("#isi_btn_confAccion").click)
+            alert("si");
+
+        //confirm("Press a button!");
+        //chkg1
+
+        return true;
     });
 
 
@@ -114,7 +160,7 @@ $(document).ready(function() {
 
         // verificamos que este visible la columna de selección múltiple
         if ($("[name='isi_td_verSiNo']").hasClass("isi_ocultar")) {
-            isi_msj_popUp.MaterialSnackbar.showSnackbar({message: "Active la opción 'MultiCheck'", timeout: 2500 // msegs
+            $isi_msj_popUp.MaterialSnackbar.showSnackbar({message: "Active la opción 'MultiCheck'", timeout: 2500 // msegs
                 // , actionHandler: function(event) {/*funcion del boton*/}
                 // , actionText: 'nombre de la accion '
             });
@@ -171,7 +217,7 @@ $(document).ready(function() {
                     $msj = "Se eliminaron " + $cantChks + " registros";
                     break;
             };
-            isi_msj_popUp.MaterialSnackbar.showSnackbar({message: $msj, timeout: 2500});
+            $isi_msj_popUp.MaterialSnackbar.showSnackbar({message: $msj, timeout: 2500});
 			// Fin mostrar mensaje toast / snack si no hubo error o si no hubo acción ajax ($$objXhr.status = 0)
 
             if ($totRegi == 0) { // si eliminan todo recargo la pagina al final
