f0f74e8d88a186e1fc08eafab1c9852dc45e18a5
adomingor <adomingor@hotmail.com>
2016-09-20 12:37:47 -0300

    terminado grupo social, pero cambiar todo a tipo de grupo social OUCH!

--- src/Isi/ConfigBundle/Controller/GrupoSocialController.php
+++ src/Isi/ConfigBundle/Controller/GrupoSocialController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Isi\ConfigBundle\Entity\GrupoSociales;
 use Isi\ConfigBundle\Form\GrupoSocialesType;
+use Nzo\UrlEncryptorBundle\Annotations\ParamDecryptor;
 
 class GrupoSocialController extends Controller
 {
@@ -51,12 +52,12 @@ class GrupoSocialController extends Controller
         }
         catch(\Doctrine\DBAL\Exception\UniqueConstraintViolationException $e) {
             $band = false;
-            $msjExtra = "Ya existe el género <b class='text-warning'>".$form->getData()->getGenero() . "</b><br>" . json_decode($this->forward('isi_mensaje:msjJson', array('id' => 3))->getContent(), true)["descrip"];
+            $msjExtra = "Ya existe el grupo social <b class='text-warning'>".$form->getData()->getGrupo() . "</b><br>" . json_decode($this->forward('isi_mensaje:msjJson', array('id' => 3))->getContent(), true)["descrip"];
             $this->forward("isi_mensaje:msjFlash", array("id" => 2, "msjExtra" => $msjExtra));
         }
         catch (\Exception $e) { // excepcion general $e->getMessage()
             $band = false;
-            $this->forward("isi_mensaje:msjFlash", array("id" => 1, "msjExtra" => "<br> <u class='text-danger'>intentando grabar el género</u>"));
+            $this->forward("isi_mensaje:msjFlash", array("id" => 1, "msjExtra" => "<br> <u class='text-danger'>intentando grabar el grupo social</u>"));
             // $this->addFlash("error", "Ups! ¬" . $e->getMessage());
         }
         return ($band);
@@ -74,5 +75,80 @@ class GrupoSocialController extends Controller
             return $this->redirectToRoute("isi_config_grupoSoc");
         }
         return $this->render("IsiConfigBundle:GrupoSocial:formulario.html.twig", array("form"=>$form->createView()));
+    }
+
+    /**
+    * @ParamDecryptor(params={"id"})
+    */
+    public function edicionAction(Request $request, $id)
+    {
+        $request->getSession()->set("icoNombre", "<i class='fa fa-pencil fa-2x isi_iconoGrupoSoc' aria-hidden='true'></i>&nbsp;<i class='fa fa-users fa-2x isi_iconoGrupoSoc' aria-hidden='true'></i>");
+        try {
+            $resu = $this->getDoctrine()->getRepository("IsiConfigBundle:GrupoSociales")->find($id);
+        } catch (\Exception $e) { // $e->getMessage()
+            $resu = null;
+            $this->forward("isi_mensaje:msjFlash", array("id" => 1, "msjExtra" => "<br> <u class='text-danger'>edicion grupos sociales (consultando)</u>"));
+            return $this->redirectToRoute("isi_config_grupoSoc");
+        }
+
+        if (!$resu){
+            $this->forward('isi_mensaje:msjFlash', array('id' => 6));
+            return $this->redirectToRoute("isi_config_grupoSoc");
+        } else {
+            $usrCrea = $resu->getUsuarioCrea(); // usuario q crea el registro
+            $ipCrea = $resu->getIpCrea(); // ip del usaurio q crea el registro
+            $fechaCrea = $resu->getFechaCrea(); // fecha y hora en que crea el registro
+            $form = $this->createForm(GrupoSocialesType::class, $resu);
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                try {
+                    $form->getData()->SetUsuarioCrea($usrCrea);
+                    $form->getData()->SetIpCrea($ipCrea);
+                    $form->getData()->SetFechaCrea($fechaCrea);
+                    $this->usrActu($form); // datos del usuario q actualiza el registro
+                    $this->getDoctrine()->getManager()->flush();
+                    $this->forward("isi_mensaje:msjFlash", array("id" => 7));
+                }
+                catch(\Doctrine\DBAL\Exception\UniqueConstraintViolationException $e) {
+                    $msjExtra = "Ya existe el grupo social <b class='text-warning'>".$form->getData()->getGrupo() . "</b><br>" . json_decode($this->forward('isi_mensaje:msjJson', array('id' => 3))->getContent(), true)["descrip"];
+                    $this->forward("isi_mensaje:msjFlash", array("id" => 2, "msjExtra" => $msjExtra));
+                }
+                catch (\Exception $e) { // excepcion general $e->getMessage()
+                    $band = false;
+                    $this->forward("isi_mensaje:msjFlash", array("id" => 1, "msjExtra" => "<br> <u class='text-danger'>intentando editar un grupo social</u>"));
+                }
+
+                return $this->redirectToRoute("isi_config_grupoSoc");
+            }
+            return $this->render("IsiConfigBundle:GrupoSocial:formulario.html.twig", array("form"=>$form->createView()));
+        }
+    }
+
+    /**
+    * @ParamDecryptor(params={"id"})
+    */
+    public function borrarAction(Request $request, $id)
+    {
+        $request->getSession()->set("icoNombre", "<i class='fa fa-trash fa-2x isi_iconoGrupoSoc' aria-hidden='true'></i>&nbsp;<i class='fa fa-users fa-2x isi_iconoGrupoSoc' aria-hidden='true'></i>");
+        try {
+            $resu = $this->getDoctrine()->getRepository("IsiConfigBundle:GrupoSociales")->find($id);
+        } catch (\Exception $e) { // $e->getMessage()
+            $resu = null;
+            $this->forward("isi_mensaje:msjFlash", array("id" => 1, "msjExtra" => "<br> <u class='text-danger'>eliminar un grupo social (consultando)</u>"));
+            return $this->redirectToRoute("isi_config_grupoSoc");
+        }
+        if (!$resu)
+            $this->forward('isi_mensaje:msjFlash', array("id" => 6));
+        else {
+            try {
+                $em = $this->getDoctrine()->getManager();
+                $em->remove($resu);
+                $em->flush();
+                $this->forward('isi_mensaje:msjFlash', array('id' => 8, "msjExtra" => "<br> <span class='text-danger'>" . $resu->getGrupo() . "</span>"));
+            } catch (\Exception $e) { // $e->getMessage()
+                $this->forward("isi_mensaje:msjFlash", array("id" => 1, "msjExtra" => "<br> <u class='text-danger'>borrarndo grupo social)</u>"));
+            }
+        }
+        return $this->redirectToRoute("isi_config_grupoSoc");
     }
 }
